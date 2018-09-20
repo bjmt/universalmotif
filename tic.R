a75047b20ee4727e70242b00312b4cbd2340c2e9
bjmt <benjmtremblay@gmail.com>
2018-09-20 16:15:17 -0400

    get rid of deploy

--- tic.R
+++ tic.R
@@ -9,9 +9,9 @@ if (Sys.getenv("id_rsa") != "") {
   get_stage("before_deploy") %>%
     add_step(step_setup_ssh())
 
-  get_stage("deploy") %>%
-    add_step(step_build_pkgdown()) %>%
-    add_step(step_push_deploy(path = "docs", branch = "gh-pages"))
+  # get_stage("deploy") %>%
+    # add_step(step_build_pkgdown()) %>%
+    # add_step(step_push_deploy(path = "docs", branch = "gh-pages"))
 }
 
 if (Sys.getenv("TRAVIS_R_VERSION") == "release") {
