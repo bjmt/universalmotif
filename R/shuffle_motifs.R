66237f2b8ba973c15d1da5df75364c52db5f5069
bjmt <benjmtremblay@gmail.com>
2019-01-09 23:49:33 -0500

    Bug fix: shuffle_motifs() produces shuffled motifs of proper length

--- R/shuffle_motifs.R
+++ R/shuffle_motifs.R
@@ -79,7 +79,7 @@ shuffle_motifs <- function(motifs, k = 2, method = "linear",
     # new.mats[[i]] <- mot.cols[, seq_len(mot.lens[i]) + mot.offsets[i]]
   # }
   new.mats <- lapply(seq_along(mot.mats),
-                     function(x) mot.cols[, seq_len(mot.lens[x] + mot.offsets[x])])
+                     function(x) mot.cols[, (1 + mot.offsets[x]):(mot.lens[x] + mot.offsets[x])])
 
   new.motifs <- mapply(shuffle_new_mot, new.mats, motifs,
                          SIMPLIFY = FALSE)
