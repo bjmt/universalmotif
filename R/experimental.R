8b84d32a6dcd3f1fd1dfb4d5f5e04d0ef7ce0ddc
bjmt <benjmtremblay@gmail.com>
2019-01-26 12:25:29 -0500

    init experimental branch

---
+++ R/experimental.R
@@ -0,0 +1,6 @@
+# UNIVERSALMOTIF CLUSTERS
+
+# Idea:
+# Have an object with multiple motifs, and some additional meta info. This meta
+# info includes total max/min width, max/min inter-motif width, motif order.
+# The idea is to allow for scanning and enriching for clusters of motifs.
