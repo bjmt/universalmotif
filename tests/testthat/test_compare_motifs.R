c080a44ab22fe852b2c321ce42229e11bfbb5db4
bjmt <benjmtremblay@gmail.com>
2019-02-10 22:58:33 -0500

    more tests: compare_motifs() + convert_type()

--- tests/testthat/test_compare_motifs.R
+++ tests/testthat/test_compare_motifs.R
@@ -24,15 +24,60 @@ test_that("basic comparison works", {
 
   expect_equal(round(as.vector(res.mpcc), digits = 4),
                c(1, 0.3333, 0.3333, 1))
-  expect_equal(as.vector(res.pcc), c(36, 12, 12, 36))
-  expect_equal(as.vector(res.eucl), c(0, 3, 3, 0))
-  expect_equal(as.vector(res.meucl), c(0, 0.5, 0.5, 0))
-  expect_equal(as.vector(res.sw), c(0, 6, 6, 0))
-  expect_equal(as.vector(res.msw), c(2, 1, 1, 2))
+  expect_equal(as.vector(res.pcc),
+               c(36, 12, 12, 36))
+  expect_equal(as.vector(res.eucl),
+               c(0, 3, 3, 0))
+  expect_equal(as.vector(res.meucl),
+               c(0, 0.5, 0.5, 0))
+  expect_equal(as.vector(res.sw),
+               c(0, 6, 6, 0))
+  expect_equal(as.vector(res.msw),
+               c(2, 1, 1, 2))
   expect_equal(round(as.vector(res.kl), digits = 2),
                c(0, 13.73, 13.73, 0))
   expect_equal(round(as.vector(res.mkl), digits = 3),
                c(0, 2.289, 2.289, 0))
+
+  res.mpcc2 <- compare_motifs(list(motif1, motif2), method = "MPCC",
+                              progress = FALSE, use.type = "ICM")
+  res.pcc2 <- compare_motifs(list(motif1, motif2), method = "PCC",
+                             progress = FALSE, use.type = "ICM")
+  res.eucl2 <- compare_motifs(list(motif1, motif2), method = "EUCL",
+                              progress = FALSE, use.type = "ICM")
+  res.meucl2 <- compare_motifs(list(motif1, motif2), method = "MEUCL",
+                               progress = FALSE, use.type = "ICM")
+  res.sw2 <- compare_motifs(list(motif1, motif2), method = "SW",
+                            progress = FALSE, use.type = "ICM")
+  res.msw2 <- compare_motifs(list(motif1, motif2), method = "MSW",
+                             progress = FALSE, use.type = "ICM")
+  res.kl2 <- compare_motifs(list(motif1, motif2), method = "KL",
+                            progress = FALSE, use.type = "ICM")
+  res.mkl2 <- compare_motifs(list(motif1, motif2), method = "MKL",
+                             progress = FALSE, use.type = "ICM")
+
+  expect_equal(round(as.vector(res.mpcc2), digits = 4),
+               c(1, 0.3846, 0.3846, 1))
+  expect_equal(round(as.vector(res.pcc2), digits = 2),
+               c(36, 13.85, 13.85, 36))
+  expect_equal(as.vector(res.eucl2),
+               c(0, 6, 6, 0))
+  expect_equal(as.vector(res.meucl2),
+               c(0, 1, 1, 0))
+  expect_equal(as.vector(res.sw2),
+               c(0, -9, -9, 0))
+  expect_equal(as.vector(res.msw2),
+               c(2, -1.5, -1.5, 2))
+  expect_equal(round(as.vector(res.kl2), digits = 2),
+               c(0, 24.23, 24.23, 0))
+  expect_equal(round(as.vector(res.mkl2), digits = 3),
+               c(0, 4.038, 4.038, 0))
+
+  res.mpcc <- compare_motifs(list(motif1, motif2), method = "MPCC",
+                             progress = FALSE, normalise.scores = TRUE)
+
+  expect_equal(round(as.vector(res.mpcc), digits = 4),
+               c(1, 0.3333, 0.3333, 1))
 
 })
 
@@ -45,5 +90,20 @@ test_that("comparisons with p-values works", {
                         method = "MPCC", progress = FALSE)
 
   expect_equal(round(res$Pval, digits = 4), 0.1601)
+
+})
+
+test_that("custom db scores are handled correctly", {
+
+  motif1 <- create_motif("TTTWWW", name = "mot1")
+  motif2 <- create_motif("GGGWWW", name = "mot2")
+
+  db.scores <- make_DBscores(c(motif1, motif2), method = "PCC",
+                             rand.tries = 10, progress = FALSE)
+
+  res <- compare_motifs(c(motif1, motif2), 1:2, db.scores, method = "PCC",
+                        normalise.scores = TRUE, max.p = 1, progress = FALSE)
+
+  expect_true(res$Pval < 1)
 
 })
--- tests/testthat/test_convert_type.R
+++ tests/testthat/test_convert_type.R
@@ -12,4 +12,20 @@ test_that("type conversion works", {
   expect_equal(m.pwm@motif[2, 2], -Inf)
   expect_equal(m.pcm@motif[1, 2], 5)
 
+  m.icm2 <- convert_type(m.pcm, "ICM")
+  m.icm3 <- convert_type(m.pcm, "ICM", nsize_correction = TRUE)
+  m.icm4 <- convert_type(m.pwm, "ICM")
+
+  expect_equal(m.icm2@motif[1, 2], 0.5)
+  expect_equal(round(m.icm3@motif[1, 2], digits = 2), 0.38)
+  expect_equal(m.icm4@motif[1, 2], 0.5)
+
+  m.ppm2 <- convert_type(m.icm, "PPM")
+  m.pcm2 <- convert_type(m.icm, "PCM")
+  m.pwm2 <- convert_type(m.icm, "PWM")
+
+  expect_equal(m.ppm2@motif[1, 2], 0.5)
+  expect_equal(m.pcm2@motif[1, 2], 5)
+  expect_equal(m.pwm2@motif[2, 2], -Inf)
+
 })
