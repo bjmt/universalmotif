1b58757516ddd93095a777e72cf9d41b742be047
Benjamin Jean-Marie Tremblay <benjmtremblay@gmail.com>
2021-01-20 16:28:01 +0100

    change warnings

--- R/universalmotif_df.R
+++ R/universalmotif_df.R
@@ -86,16 +86,16 @@ update_motifs <- function(motif_df) {
   if (any(!cols_new %in% cols_old)) {
     # maybe remove this warning based on how extrainfo is implemented
     warning(
-      "Removing unknown slots:\n",
-      paste0(paste0("* ", cols_new[!cols_new %in% cols_old]), collapse = "\n"),
+      "Removing unknown slot(s) ",
+      paste0(paste0("'", cols_new[!cols_new %in% cols_old], "'"), collapse = ", "),
       immediate. = TRUE, call. = FALSE
     )
   }
   if (any(!cols_old %in% cols_new)) {
     # hide this warning when called in to_list()?
     warning(
-      "Restoring missing slot:\n",
-      paste0(paste0("* ", cols_old[!cols_old %in% cols_new]), collapse = "\n"),
+      "Restoring missing slot(s) ",
+      paste0(paste0("'", cols_old[!cols_old %in% cols_new], "'"), collapse = ", "),
       immediate. = TRUE, call. = FALSE
     )
   }
@@ -107,7 +107,7 @@ update_motifs <- function(motif_df) {
     old_df[[checking]][is.na(old_df[[checking]])] <- 0
     if (any(updated_df[[checking]] != old_df[[checking]])) {
       if (checking %in% c("icscore", "consensus", "alphabet")) {
-        warning("Ignoring changes in unmodifiable slot '", checking, "'",
+        warning("Ignoring changes in unmodifiable slot(s) '", checking, "'",
           immediate. = TRUE, call. = FALSE)
       } else {
         for (j in which(updated_df[[checking]] != old_df[[checking]])) {
